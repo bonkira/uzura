124f503dbb38831f626330cbc0a5a96b956963a9
千葉菜々美 <nanami-chiba@navitime.co.jp>
2023-05-23 14:50:23 +0900

    build成果物を配置するディレクトリ変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  // `mode` に基づいて現在の作業ディレクトリにある env ファイルをロードする
+  // `VITE_` プレフィックスに関係なく全ての環境変数をロードするには、第 3 引数に '' を設定します
+  const env = loadEnv(mode, process.cwd(), '')
+  if (command === 'serve') {
+    return {
+      // localhost固有の設定
+      server: {
+        proxy: {
+          "/bff": {
+            target: "http://localhost:8080/tsubame/bff",
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/bff/, ""),
+          },
+        },
+      },
+      plugins: [vue()]
+    }
+  } else {
+    return {
+      plugins: [vue()]
+    }
+  }
 })
